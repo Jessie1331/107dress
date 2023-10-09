f9c6875cfe14e9584d9af2fd0d2f2e4b3db1fef0
Jessie1331 <141692878+Jessie1331@users.noreply.github.com>
2023-10-09 02:12:11 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { React } from "react"
+import Navbar from "./components/navbar";
+import Footer from "./components/footer"
+function App() {
+   (<div className='App'>
+    <Navbar/>
+    
+    <Footer/>
+       </div>
+  
+
+  );
+}
+
+export default App;
